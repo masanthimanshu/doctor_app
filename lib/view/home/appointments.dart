8d9bededfa01b912011adae57a00781995c244e0
Himanshu Masant <masanthimanshu@gmail.com>
2023-07-11 17:32:38 +0530

    Updates

--- lib/view/home/appointments.dart
+++ lib/view/home/appointments.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentsScreen extends StatelessWidget {
@@ -26,6 +27,266 @@ class AppointmentsScreen extends StatelessWidget {
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: Row(
+            children: [
+              Container(
+                width: 75,
+                height: 75,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topCenter,
+                    image: CachedNetworkImageProvider(
+                      "https://images.unsplash.com/photo-1524504388940-b1c1722653e1",
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "User Name",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "Cardiologist",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 8),
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blue.shade200,
+                        ),
+                        child: Icon(
+                          size: 15,
+                          color: Colors.blue[800],
+                          Icons.medication_rounded,
+                        ),
+                      ),
+                      const Text("3 Years"),
+                      const SizedBox(width: 10),
+                      Container(
+                        margin: const EdgeInsets.only(right: 8),
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red.shade200,
+                        ),
+                        child: Icon(
+                          size: 15,
+                          Icons.favorite,
+                          color: Colors.red[800],
+                        ),
+                      ),
+                      const Text("67%"),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 10,
+          color: Colors.grey[200],
+          width: double.infinity,
+        ),
+        const Padding(
+          padding: EdgeInsets.all(15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Total Cost",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Session fee for 30 min",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "\$ 80",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.indigo,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "To Pay",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.indigo,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "\$ 80",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.indigo,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 10),
+        const Divider(thickness: 2),
+        const SizedBox(height: 10),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          margin: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(150),
+            color: Colors.blue[100],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.percent,
+                  color: Colors.white,
+                  size: 15,
+                ),
+              ),
+              Text(
+                "Use Promo Code",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.indigo[800],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_forward_ios),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 20),
+        Container(
+          height: 10,
+          color: Colors.grey[200],
+          width: double.infinity,
+        ),
+        const Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Payment Options",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.indigo,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            border: Border.all(
+              width: 2,
+              color: Colors.grey,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Radio(
+                    value: "value",
+                    groupValue: "groupValue",
+                    onChanged: (val) {},
+                  ),
+                  const Text(
+                    "PayPal",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(
+                thickness: 2,
+                color: Colors.grey,
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: "value",
+                    groupValue: "groupValue",
+                    onChanged: (val) {},
+                  ),
+                  const Text(
+                    "Credit Card",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
--- lib/view/home/doctors.dart
+++ lib/view/home/doctors.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class DoctorsScreen extends StatefulWidget {
@@ -81,6 +82,114 @@ class _DoctorsScreenState extends State<DoctorsScreen> {
             ],
           ),
         ),
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: Row(
+            children: [
+              Container(
+                width: 75,
+                height: 75,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topCenter,
+                    image: CachedNetworkImageProvider(
+                      "https://images.unsplash.com/photo-1524504388940-b1c1722653e1",
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "User Name",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "Cardiologist",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 8),
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.blue.shade200,
+                        ),
+                        child: Icon(
+                          size: 15,
+                          color: Colors.blue[800],
+                          Icons.medication_rounded,
+                        ),
+                      ),
+                      const Text("3 Years"),
+                      const SizedBox(width: 10),
+                      Container(
+                        margin: const EdgeInsets.only(right: 8),
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red.shade200,
+                        ),
+                        child: Icon(
+                          size: 15,
+                          Icons.favorite,
+                          color: Colors.red[800],
+                        ),
+                      ),
+                      const Text("67%"),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: Row(
+            children: [
+              const Column(
+                children: [
+                  Text(
+                    "Total Fees",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "\$ 80",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Make an appointment"),
+                ),
+              ),
+            ],
+          ),
+        ),
         Container(
           height: 10,
           color: Colors.grey[200],
@@ -105,6 +214,126 @@ class _DoctorsScreenState extends State<DoctorsScreen> {
               ),
             ],
           ),
+        ),
+        ListView.builder(
+          itemCount: 5,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (e, index) {
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 75,
+                        height: 75,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            alignment: Alignment.topCenter,
+                            image: CachedNetworkImageProvider(
+                              "https://images.unsplash.com/photo-1524504388940-b1c1722653e1",
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "User Name",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.indigo,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const Text(
+                            "Cardiologist",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(right: 8),
+                                padding: const EdgeInsets.all(4),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.blue.shade200,
+                                ),
+                                child: Icon(
+                                  size: 15,
+                                  color: Colors.blue[800],
+                                  Icons.medication_rounded,
+                                ),
+                              ),
+                              const Text("5 Years"),
+                              const SizedBox(width: 10),
+                              Container(
+                                margin: const EdgeInsets.only(right: 8),
+                                padding: const EdgeInsets.all(4),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.red.shade200,
+                                ),
+                                child: Icon(
+                                  size: 15,
+                                  Icons.favorite,
+                                  color: Colors.red[800],
+                                ),
+                              ),
+                              const Text("84%"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      const Column(
+                        children: [
+                          Text(
+                            "Total Fees",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "\$ 80",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Make an appointment"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(thickness: 2),
+              ],
+            );
+          },
         ),
       ],
     );
